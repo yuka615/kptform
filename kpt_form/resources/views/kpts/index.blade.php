9301cf951dd8d3ee4112f6add6f9c020db7e7a80
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-06 02:00:59 +0000

    bootstrap1

--- kpt_form/resources/views/kpts/index.blade.php
+++ kpt_form/resources/views/kpts/index.blade.php
@@ -2,16 +2,34 @@
 
 @section('content')
 
-    <h1>KPT form</h1>
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
-    @if (count($kpts) > 0)
-        <ul>
-            @foreach ($kpts as $kpt)
-                 <li>{!! link_to_route('kpts.show', $kpt->id, ['id' => $kpt->id]) !!} : {{ $kpt->title }} > {{ $kpt->content }}</li>
-            @endforeach
-        </ul>
-    @endif
+            <h1>KPT form</h1>
+        
+            @if (count($kpts) > 0)
+            <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>dates</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($kpts as $kpt)
+                            <tr>
+                                <td>{!! link_to_route('kpts.show', $kpt->id, ['id' => $kpt->id]) !!}</td>
+                                <td>{!! link_to_route('kpts.show', $kpt->dates, ['id' => $kpt->id]) !!}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+            </table>
+            
+            @endif
     
-    {!! link_to_route('kpts.create', '新規KPTの入力') !!}
+            {!! link_to_route('kpts.create', '新規KPTの入力') !!}
+    
+        </div>
+    </div>
 
 @endsection
