97cf0587eec72ba05c1f20e3086e491c552849b0
nana.shimizu <nana.shimizu@rakuten.com>
2018-06-06 06:33:22 +0000

    show bootstrap

--- kpt_form/resources/views/kpts/show.blade.php
+++ kpt_form/resources/views/kpts/show.blade.php
@@ -1,16 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
+    
+            <div class="alert alert-info" role="alert">
+                    今日は、<b>
+                        <?php 
+                            $timestamp = time() ;
+                            echo date( "Y/m/d" , $timestamp ) ;
+                        ?>
+                    </b>です。
+            	        <?php
+            	            function rand_message() {
+                            	$message = array("一日頑張りましょう！", "適度に休憩しましょうね。", "一つ一つこなしていきましょう！", "今日も三度の飯より PHP です。", "コーヒーの飲みすぎに注意！");
+                            	return $message[array_rand($message)];
+                            }
+             
+                            echo rand_message();
+            	        ?>
+        	  </div>
+    
+    <h1>{{ $kpt->dates }} の詳細ページ</h1>
+    
+    <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Dates</th>
+                            <th>Contents</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <tr>
+                                <td><b>{{ $kpt->dates }}</b></td>
+                                <td>
+                                    <p><b>Keep:</b><br> {!! nl2br($kpt->keep) !!}</p>
+                                    <p><b>Problem:</b><br> {!! nl2br($kpt->problem) !!}</p>
+                                    <p><b>Try:</b><br> {!! nl2br($kpt->Try) !!}</p>
+                                    <p><b>MVP:</b><br> {!! nl2br($kpt->MVP) !!}</p>
+                                    <p><b>Reason:</b><br> {!! nl2br($kpt->reason) !!}</p>
+                                    <p><b>Want:</b><br> {!! nl2br($kpt->want) !!}</p>
+                           
+                                
+                                </td>
+                            </tr>
+                    </tbody>
+            </table>
+    </div>
+<div class='btn-toolbar'>
+    {!! link_to_route('kpts.edit', ' このKPTを編集', ['id' => $kpt->id], ['class' => 'btn btn-primary glyphicon glyphicon-edit']) !!}
 
-    <h1>id = {{ $kpt->id }} の詳細ページ</h1>
-
-    <p>タイトル: {{ $kpt->title }}</p>
-    <p>Keep: {{ $kpt->keep }}</p>
-    <p>Problem: {{ $kpt->problem }}</p>
-
-    {!! link_to_route('kpts.edit', 'このKPTを編集', ['id' => $kpt->id]) !!}
-
-    {!! Form::model($kpt, ['route' => ['kpts.destroy', $kpt->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+    {!! Form::model($kpt, ['route' => ['kpts.destroy', $kpt->id], 'method' => 'delete'])!!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger pull-right']) !!}
     {!! Form::close() !!}
+</div>
+
+    
 @endsection
